9c32a574f811b73b6afd64c6c55b2082a5838941
Elliot Chi <elliotschi@gmail.com>
2016-01-15 16:51:14 -0800

    feat(server): add database connection

---
+++ server/src/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "FACEBOOK_APP_ID": "1515389662089605",
+  "FACEBOOK_APP_SECRET": "8dd6ad54d166c5311c5ff13fd9f0f51a",
+  "neo4j": "https://569696eff1867:EL8VYJjyNEugQoB8569I955DfAyZ0p8vFtA4OwDK@neo-greta-murphy-turquoise-5696964872e32.do-stories.graphstory.com:7473" 
+}
