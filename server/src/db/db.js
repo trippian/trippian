4120be2e92c9a03273bcc160ec7a272712536721
vidaaudrey <audrey@33road.com>
2016-01-17 12:26:21 -0800

    fix(server): fix the config file path problem so the travis CI test can pass

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,6 +1,6 @@
 const neo4j = require('node-neo4j');
-var config = require('../config/config.js');
-var DATABASE_URI = process.env.GRAPHSTORY_URL || require('../config/config.js').neo4j;
+var config = require('../../../src/shared/config/config')
+var DATABASE_URI = process.env.GRAPHSTORY_URL || require('../../../src/shared/config/config').neo4j;
 var db = new neo4j(DATABASE_URI);
 
 module.exports = db;
