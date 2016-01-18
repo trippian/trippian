27ece7e94675468695b3fe6286ad2cb820503170
Elliot Chi <elliotschi@gmail.com>
2016-01-17 16:58:28 -0800

    feat(server): working on all database models

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,6 +1,10 @@
-const neo4j = require('node-neo4j');
-var config = require('../config/config.js');
-var DATABASE_URI = process.env.GRAPHSTORY_URL || require('../config/config.js').neo4j;
-var db = new neo4j(DATABASE_URI);
+const Seraph = require('seraph');
+const Promise = require('bluebird');
+let config = require('../config/config.js');
+let DATABASE_URI = process.env.GRAPHSTORY_URL || require('../config/config.js').neo4j;
+
+let db = Seraph(DATABASE_URI);
+
+db = Promise.promisifyAll(db);
 
 module.exports = db;
--- server/src/db/models/user.js
+++ server/src/db/models/user.js
@@ -4,6 +4,31 @@ const db = require('../db');
 
 module.exports = {
   createUser: function(username) {
-    db.cypher()
-  }
+    
+  },
+  getUserById: function(id) {
+    return new Promise(function(resolve) {
+      let cypher = 'match (user:User) where user.facebookId=' + id ' return user';
+      db.queryAsync(cypher)
+        .then(function(user) {
+          if (user) {
+            resolve(user);
+          } reject('user does not exist');
+        });
+    });
+  },
+  // gets a user when searching by a certain parameter ie. field would equal facebookId and value would be the id
+  getUserByParameter: function(field, value) {
+    return new Promise(function(resolve, reject) {
+      let cypher = 'match (user:User) where ' + field + '=' + value + ' return user';
+      db.queryAsync(cypher)
+        .then(function(user) {
+          if (user) {
+            resolve(user);
+          } reject('user does not exist with that value');
+        });
+    });
+  },
+
+
 }
