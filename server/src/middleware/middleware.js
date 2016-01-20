13d1ba4a83c3dd874f9b2f4e67d0a3979b6deec9
Elliot Chi <elliotschi@gmail.com>
2016-01-20 14:54:55 -0800

    feat(server): work on passport

--- server/src/middleware/middleware.js
+++ server/src/middleware/middleware.js
@@ -1,34 +1,44 @@
-import { json, urlencoded } from 'body-parser';
-import { join } from 'path';
-import { errorHandler, errorLogger } from './utils';
-import Router from '../routes/routes';
-import morgan from 'morgan';
+import { json, urlencoded } from 'body-parser'
+import { join } from 'path'
+import { errorHandler, errorLogger } from './utils'
+import Router from '../routes/routes'
+import AuthRouter from '../routes/authRoutes'
+import morgan from 'morgan'
+import passport from 'passport'
+import PassportConnection from '../passport'
+import session from 'express-session'
 // import graphqlHTTP from 'express-graphql';
 // import MyGraphQLSchema from '../graphQL/graphQLSchema';
 
 export default function (app, express) {
-  let router = express.Router();
+  let router = express.Router()
+  let authRouter = express.Router()
 
-  app.use(morgan('dev'));
-  app.use(json());
-  // app.use(urlencoded({extended: true}));
+  PassportConnection(passport)
+  app.use(morgan('dev'))
+  app.use(json())
 
-  // app.use(express.static(join(__dirname, 'dist')));
+  app.use(express.static(join(__dirname, 'deploy')));
 
   // graph ql route => we will use this later
   // app.use('/api', graphqlHTTP({schema: MyGraphQLSchema, graphiql: true }));
-  // app.use('/', function(req, res) {
-  //   res.send('Hello');
-  // });
 
-  app.use('/', router);
+  app.use(session({secret: 'no time to eat'}))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  // not sure if i need this yet
+  // app.use(flash())
+
+  app.use('/', router)
+  app.use('/auth', authRouter)
 
   app.use('*', function(req, res) {
-    res.status(404).send('404: Page not found');
-  });
+    res.status(404).send('404: Page not found')
+  })
 
-  Router(router);
+  Router(router)
+  AuthRouter(authRouter, passport)
 
-  app.use(errorHandler);
-  app.use(errorLogger);
+  app.use(errorHandler)
+  app.use(errorLogger)
 }
