ef6eb1b487c55d3f8e393031e09fa466f41dabc8
Elliot Chi <elliotschi@gmail.com>
2016-01-21 12:12:24 -0800

    feat(server): working on inquiries and routes for inquiries

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,30 +1,31 @@
-import express from 'express';
+import express from 'express'
 import {
   getPopularDestinations
 }
-from '../controllers/destinationController';
+from '../controllers/destinationController'
 import {
   createUser, getPopularTrippians
 }
-from '../controllers/userController';
+from '../controllers/userController'
 import {
   getSignedRequest
 }
-from '../controllers/aws';
+from '../controllers/aws'
 // import { authController } from '../controllers/auth';
-import destination from '../../../_planning/json-data-format/destination.json';
-import trippian from '../../../_planning/json-data-format/trippian.json';
+import destination from '../../../_planning/json-data-format/destination.json'
+import trippian from '../../../_planning/json-data-format/trippian.json'
 
 export default function (app) {
   // LANDING PAGE - GET POPULAR TRIPPIANS AND DESTINATIONS
-  // app.get('/destination', getPopularDestinations);
-  // app.get('/trippian', getPopularTrippians);
-  app.post('/api/users/:facebookId', createUser);
+  app.get('/api/destination', getPopularDestinations)
+  app.get('/api/trippian', getPopularTrippians)
+  app.post('/api/users/:facebookId', createUser)
+  app.post('/api/inquiry/:')
   app.get('/api/destination/:destinationId', function (req, res) {
-    res.send(destination);
-  });
+    res.send(destination)
+  })
   app.get('/api/trippian/:userId', function (req, res) {
-    res.send(trippian);
+    res.send(trippian)
   })
 
   // FACEBOOK AUTH
@@ -32,5 +33,5 @@ export default function (app) {
   // app.get('/auth/facebook/callback', authController.facebookCallback, authController.validate);
 
   // AMAZON S3
-  app.get('/signS3', getSignedRequest);
+  app.get('/signS3', getSignedRequest)
 }
