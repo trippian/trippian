e9d8685ca738b3b82e6c0615197aa1ef568624df
Elliot Chi <elliotschi@gmail.com>
2016-01-21 12:28:00 -0800

    refactor(server): configure server for dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ import app from './server-config.js';
 
 // test on env config, remove later 
 require('dotenv').config();
-console.log('env', process.env.neo4j, process.env.HOST, process.env.FACEBOOK_APP_ID);
 
 let port = process.env.PORT || 4000;
 
