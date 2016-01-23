98aabca255d9c8823b9e630950f725c235170fcd
Yale Yuen <yaleyyuen@gmail.com>
2016-01-22 17:23:11 -0800

    server(feat) working on inquiryPost

--- server/src/controllers/inquiryController.js
+++ server/src/controllers/inquiryController.js
@@ -2,19 +2,27 @@ import Inquiry from '../db/models/inquiry'
 import User from '../db/models/user'
 
 export default {
-  inquiryGet: function (req, res, next) {
+  inquiryGet: (req, res, next) => {
     let userId = req.params.userId;
     if (userId) {
       User.getUserByParameter("id", userId)
-        .then(function(inquiry) {
+        .then((inquiry) => {
           Inquiry.getAllInquiriesForTrippian(userId)
           if (inquiry) {
             res.json(inquiry)
           }
         })
-        .catch(function (error) {
+        .catch((error) => {
           next(error)
         })
+    }
+  },
+  inquiryPost: function(req, res, next) {
+    let postDetails = req.body;
+    let d = req.params;
+    console.log(d,'.params')
+    if (postDetails) {
+      Inquiry.createInquiry()
     }
   }
 }
