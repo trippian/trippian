54d027d1740d5e2d2802de8c102c5835f4ae11f4
Joe Lagasse <joe@servant.co>
2016-01-23 19:32:17 -0500

    feat(client): implement flow of dummy data on destination path

--- src/client/components/TrippianListItemWidget/TrippianListItemWidget.js
+++ src/client/components/TrippianListItemWidget/TrippianListItemWidget.js
@@ -9,22 +9,22 @@ import {
 from '../index'
 
 const TrippianListItemWidget = ({
-  name = 'Amanda . Sydney'
+  name, profilePicture, averageRating
 }) => {
   return (
     <div className="trippian-list-item row">
         <div className="col-xs-6 col-sm-2 col-md-2 col-xs-offset-3 col-sm-offset-0">
-            <CircleImageWidget />
+            <CircleImageWidget imgSrc={profilePicture} />
         </div>
         <div className="col-xs-12 col-sm-8 col-md-8 col-lg-8">
             <div className="title-section">
-                <h4>Sarah Johns</h4>
+                <h4>{name}</h4>
                 <span>Sydney, Paris, New York</span>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur debitis soluta dolor ratione veniam alias sapiente dolore ullam. Error, molestiae nesciunt. Praesentium iste deserunt nemo.</p>
             </div>
         </div>
         <div className="col-xs-12 col-sm-2 col-md-2 col-lg-2">
-          <StarRatingWidget />
+          <StarRatingWidget stars={averageRating}/>
           <ContactButtonWidget />
         </div>
     </div>
