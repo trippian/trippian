dd4d940d4197c4dd661d3627fedda39e018432c5
Joe Lagasse <joe@servant.co>
2016-01-29 13:42:56 -0500

    bug(client): fix merge conflict

--- src/client/components/DestinationListItemWidget/DestinationListItemWidget.js
+++ src/client/components/DestinationListItemWidget/DestinationListItemWidget.js
@@ -11,23 +11,14 @@ const styles = {
 }
 
 const DestinationListItemWidget = ({
-  destinationName, destinationImage, id
+  destinationName, id
 }) => {
   return (
-<<<<<<< HEAD
-    <div className="col-xs-12 col-sm-6 col-md-4 popular-destinations-item
-    ">  
-        <Link to={`destination/${id}`} className="thumbnail text-center" style = {
-            styles.backgroundImage
-            }>
-            <span>{destinationName}</span>
-=======
     <div className="col-xs-12 col-sm-6 col-md-4 popular-destinations-item">  
         <Link to={`destination/${id}`} className="thumbnail text-center" style = {
             styles.backgroundImage
             }>
             {destinationName}
->>>>>>> bug(client): fix files lost from reset
         </Link>
       </div>
   )
@@ -35,8 +26,4 @@ const DestinationListItemWidget = ({
 
 DestinationListItemWidget.displayName = 'DestinationListItemWidget'
 
-<<<<<<< HEAD
 export default DestinationListItemWidget
-=======
-export default DestinationListItemWidget
->>>>>>> bug(client): fix files lost from reset
