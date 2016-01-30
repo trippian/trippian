f286d698fcf35ea3cbd3ca9dee263306eb43052b
Yale Yuen <yaleyyuen@gmail.com>
2016-01-29 16:10:52 -0800

    feat(client): implement review widget

--- src/client/components/ReviewListWidget/ReviewListWidget.js
+++ src/client/components/ReviewListWidget/ReviewListWidget.js
@@ -1,20 +1,41 @@
 import React from 'react'
-
 import {
   ReviewListItemWidget
 }
 from '../index'
 
+var reviewList = [{
+    "name": 'Yale Yuen',
+    "profilePicture": 'http://lorempixel.com/400/200/animals/',
+    "rating": 1,
+    "review": 'He servicable defense but no potential.'
+  },
+
+  {
+    "name": 'Josh Smith',
+    "profilePicture": 'http://lorempixel.com/400/200/animals/',
+    "rating": 2,
+    "review": 'Sick baller. Decent handles for a 4'
+  },
+
+  {
+    "name": 'Steph Curry',
+    "profilePicture": 'http://lorempixel.com/400/200/animals/',
+    "rating": 1,
+    "review": 'Garbage player says Elliot.'
+  }
+]
+
 const ReviewListWidget = ({
-  name = 'ReviewListWidget'
+  reviews = reviewList
 }) => {
   return (
     <div className="section-body review-list">
-        <ReviewListItemWidget />
-        <ReviewListItemWidget />
-        <ReviewListItemWidget />
-        <ReviewListItemWidget />
-        <ReviewListItemWidget />
+        {
+          reviews.map((review,key) => 
+            <ReviewListItemWidget key={key} {...review} />
+          )
+        }
     </div>
   )
 }
