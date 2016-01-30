c5b8ecaa5f9426da6c50ac47dbbd123e5aa6253c
vidaaudrey <audrey@33road.com>
2016-01-30 11:23:31 -0800

    feat(client): add relativeTime to review

--- src/client/components/ReviewListItemWidget/ReviewListItemWidget.js
+++ src/client/components/ReviewListItemWidget/ReviewListItemWidget.js
@@ -4,7 +4,7 @@ import {
 }
 from 'react-router'
 import {
-  CircleImageWidget, ContactButtonWidget, StarRatingWidget
+  CircleImageWidget, ContactButtonWidget, StarRatingWidget, RelativeTimeWidget
 }
 from '../index'
 
@@ -19,8 +19,14 @@ const ReviewListItemWidget = ({
         <div className="col-xs-12 col-sm-10 col-md-10 text-expandable">
             <h4>{title}</h4>
             <div className="meta"> 
-             <a href="#" title="">{username}</a><i>3d ago</i><StarRatingWidget stars={rating}/>  
+             <a href="#" title="">{username}</a>
+             <i>
+              <RelativeTimeWidget date={new Date('2016-01-30T04:13:28.003')} intl='en-US' />
+              </i>
+              <StarRatingWidget stars={rating}/>  
             </div>
+            
+            
             {content}                                  
         </div>            
     </div>
