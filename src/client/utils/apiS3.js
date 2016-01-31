fe584821ffdc7e5d596551124a14ec574b587837
vidaaudrey <audrey@33road.com>
2016-01-30 16:54:27 -0800

    feat(client): add upload component

---
+++ src/client/utils/apiS3.js
@@ -0,0 +1,21 @@
+import api from '../../shared/api/fetch'
+import {
+  API_HOST, Server_HOST, routeConfig
+}
+from '../config/appConfig'
+
+export const getS3SignedURL = (file) => {
+  const url = `${Server_HOST}${routeConfig.s3}?file_name=${file.name}&file_type=${file.type}`
+  console.log('getS3SignedURL', url)
+  return api.get(url)
+}
+
+export const putS3File = (signedUrl, file) => {
+  console.log('posting file to S3 now', signedUrl)
+  return api.putFile(signedUrl, file)
+}
+
+export default {
+  getS3SignedURL,
+  putS3File
+}
