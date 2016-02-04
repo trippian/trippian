efc068ffba4734c136b50431dcce5b0424deca1b
vidaaudrey <audrey@33road.com>
2016-02-04 10:47:57 -0800

    feat: add myTripBox to dashboard

--- src/client/components/TripListWidget/TripListWidget.js
+++ src/client/components/TripListWidget/TripListWidget.js
@@ -5,16 +5,20 @@ import {
 from '../index'
 
 const TripListWidget = ({
-  dataList = []
+  dataList = [], emptyMessage = 'There is no trip yet. '
 }) => {
   console.log('inside Trip list', dataList, dataList.length)
   return (
     <div className="popular-Trips section-body clearfix">
-    { 
-       dataList.map((trip, key) => (
-         <TripListItemWidget key={key} {...trip} />
-      ))
-   }
+      {dataList.length === 0 && 
+        <h3 className="no - list - message
+">{emptyMessage}</h3>
+      }
+      { 
+         dataList.map((trip, key) => (
+           <TripListItemWidget key={key} {...trip} />
+        ))
+      }
     </div>
   )
 }
