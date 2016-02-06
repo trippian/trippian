044d996dabc2d8e5a9381648d7e2596b9f0f15ad
Elliot Chi <elliotschi@gmail.com>
2016-02-06 15:39:39 -0800

    fix: routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,6 @@
 import auth from '../controllers/auth'
 
 export default function(router) {
-  // router.get('/facebook', passport.authenticate('facebook', {
-  //   scope: ['email']
-  // }))
   
   router.post('/signup', auth.signup, auth.createCookie)
 
@@ -11,32 +8,7 @@ export default function(router) {
   
   router.get('/facebook', auth.facebook)
 
-  // router.get('/facebook/callback', passport.authenticate('facebook', {
-  //   successRedirect: '/',
-  //   failureRedirect: '/'
-  // }), function(req ,res) {
-  //   console.log('hi')
-  //   res.cookie('trippian', {
-  //     id: req.user.id,
-  //     name: req.user.name
-  //   })
-  // })
   router.get('/facebook/callback', auth.facebookCallback, auth.validateFacebook)
-
-  // router.get('/google', passport.authenticate('google', {
-  //   scope: ['profile', 'email']
-  // }))
-
-  // router.get('/google/callback', passport.authenticate('google', {
-  //   successRedirect: '/',
-  //   failureRedirect: '/'
-  // }), function(req ,res) {
-  //   console.log('hi')
-  //   res.cookie('trippian', {
-  //     id: req.user.id,
-  //     name: req.user.name
-  //   })
-  // })
 
   router.get('/google', auth.google)
   router.get('/google/callback', auth.googleCallback, auth.validateGoogle)
