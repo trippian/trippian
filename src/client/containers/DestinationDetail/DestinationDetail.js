6b630c57de66fe3bc0b71113ef92eb42853a051f
Joe Lagasse <joe@servant.co>
2016-02-05 20:31:57 -0500

    chore(client): fix styles

--- src/client/containers/DestinationDetail/DestinationDetail.js
+++ src/client/containers/DestinationDetail/DestinationDetail.js
@@ -7,7 +7,7 @@ import {
 }
 from 'react-redux'
 import {
-  JumbotronDestinationWidget, CarouselWidget, SectionHeaderWidget, TrippianListWidget, TripListWidget, GoogleMapWidget
+  JumbotronDestinationWidget, SectionHeaderWidget, TrippianListWidget, TripListWidget, GoogleMapWidget
 }
 from '../../components/index'
 import {
@@ -36,7 +36,6 @@ export default class DestinationDetail extends Component {
     })
   }
   render() {
-      console.log('inside destination detail render', this.props.destination)
       const {
         whyVisit, description, popularTrips, album, lat = '', lng = ''
       } = this.props.destination
@@ -54,22 +53,11 @@ export default class DestinationDetail extends Component {
             <SectionHeaderWidget title="Why Visit" subTitle="" />
             <div className="section-body">
               {whyVisit}
-              <button type="button" className='btn-link' onClick={this.showReadMore.bind(this)} ria-label="Read More">more</button>
               {this.state.isShowReadMore && <p>{description}</p> }
             </div>
           </div>
         </div>
       </div>
-      
-      {album && 
-        <div className="row section">
-          <SectionHeaderWidget title="Photos" subTitle="Lorem ipsum dolor." />
-          <div className="section-body">
-              <CarouselWidget dataList={album}/>
-          </div>
-        </div>
-      }
-
       <div className="row section">
         <SectionHeaderWidget title="Popular Trips" subTitle="Lorem ipsum dolor." />
         <div className="section-body">
